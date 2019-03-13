2bea998da241accc3e37d1cb21b9024485e1918b
Artem Gorokhov <artem.gorohov@gmail.com>
2019-03-13 16:07:06 +0300

    Redirect to login page

--- webserver/app.py
+++ webserver/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/')
+
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    if True:
+        return redirect(url_for('login'))
+    return app.send_static_file('index.html')
+
+@app.route("/login")
+def login():
+    # return send_from_directory('login', 'login.html')
+    return send_from_directory(os.path.join(app.static_folder, 'login'), 'login.html')
