6f73720d0c3ee97bfddbba49c9e25b2a5afafff3
Artem Gorokhov <artem.gorohov@gmail.com>
2019-03-14 14:59:40 +0300

    require auth at index page

--- webserver/routes.py
+++ webserver/routes.py
@@ -1,16 +1,15 @@
+from webserver import app
 from flask import Flask, render_template, redirect, url_for, send_from_directory
+from webserver.auth import requires_auth
 import os
-
-app = Flask(__name__, 
-            static_folder='../dist/static',
-            template_folder='../dist')
-
-
-@app.route("/")
-def index():
-    return render_template('index.html')
 
 @app.route("/login")
 def login():
     # return send_from_directory('login', 'login.html')
+    print('Folder: {}'.format(os.path.join(app.static_folder, 'login')))
     return send_from_directory(os.path.join(app.static_folder, 'login'), 'login.html')
+
+@app.route("/")
+@requires_auth
+def index():
+    return render_template('index.html')
