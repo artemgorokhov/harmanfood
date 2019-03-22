3dcba47d4109b01c7266766e86bab2ae4e88fc67
artemgorokhov <48455754+artemgorokhov@users.noreply.github.com>
2019-03-23 00:28:09 +0300

    Webpack login (#4)
    * trying to launch devserver for 2 entries
    
    * Devserver for login page (npm run login)
    
    * Use login js code inside vue file
    
    * separate js from login logic

---
+++ frontend/src/components/login/login_form.vue
@@ -0,0 +1,40 @@
+<template>
+    <form>
+    Login <input type="text" 
+                 v-model="login" 
+                 v-on:input="changeSubmitStatus"
+                 placeholder="LOGIN"/>
+            <br>
+    Password <input 
+                 type="text" 
+                 v-model="passwd" 
+                 v-on:input="changeSubmitStatus"
+                 placeholder="PASSWORD"/>
+            <br>
+    <button v-on:click="submit" 
+            type="button" 
+            class="btn" 
+            ref="submitButton" disabled>Submit</button>
+  </form>
+</template>
+
+<script>
+import { submitForm, formIsInvalid } from '@/assets/js/login_form.js'
+
+export default {
+  name: 'login-form',
+  data: () => ({
+    login: '',
+    passwd: ''
+  }),
+  methods: {
+    submit: function() {
+        submitForm(this.$data.login, this.$data.passwd);
+    },
+    changeSubmitStatus: function() {
+        this.$refs.submitButton.disabled = formIsInvalid(this.$data.login, this.$data.passwd);
+    }
+  }
+}
+
+</script>
