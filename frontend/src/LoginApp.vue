241fa75064bdfdaa7df8648450fd5b1425b72d77
Artem Gorokhov <artem.gorohov@gmail.com>
2019-03-22 19:21:41 +0300

    Use login js code inside vue file

--- frontend/src/LoginApp.vue
+++ frontend/src/LoginApp.vue
@@ -7,9 +7,61 @@
 </template>
 
 <script>
-export default {
-  name: 'LoginApp'
+
+// import { submitForm, eaterCredentials } from '@/assets/js/login_form.js'
+import axios from 'axios'
+
+var eaterCredentials = {
+  login: '',
+  passwd: ''
 }
+
+var formIsInvalid = function () {
+  return !(eaterCredentials.login.length && eaterCredentials.passwd.length)
+}
+
+var submitForm = function () {
+  console.log('Submit button clicked')
+  if (!formIsInvalid()) {
+    axios.post('/api/login', {
+      username: eaterCredentials.login,
+      password: eaterCredentials.passwd
+    })
+      .then(function (response) {
+        if (!response.data.hasOwnProperty('result')) {
+          console.log('Response without result...')
+        }
+        if (response.data.result === 'success') {
+          console.log('All right, redirecting to home page')
+          window.location.href = '/'
+        } else {
+          console.log('Wrong credentials!')
+        }
+      })
+      .catch(function (error) {
+        console.log('Error: ' + error)
+      })
+  }
+}
+
+var enableSubmitBtn = function () {
+    this.$refs.submitButton.disabled = formIsInvalid();
+};
+
+export default {
+  name: 'LoginApp',
+  data: () => (
+    eaterCredentials
+  ),
+  methods: {
+    submit: submitForm
+  },
+  watch: {
+    login: enableSubmitBtn,
+    passwd: enableSubmitBtn
+  }
+}
+
 </script>
 
 <style lang="sass">
--- frontend/src/assets/js/login_form.js
+++ frontend/src/assets/js/login_form.js
@@ -1,67 +1,41 @@
-/*jslint browser */
-/*global window */
+/* jslint browser */
+/* global window */
 
-window.onload = function () {
-    "use strict";
+'use strict'
 
-    var vm = null;
+import axios from 'axios'
 
-    var eater_credentials = {
-        login: "",
-        passwd: ""
-    };
+var eaterCredentials = {
+  login: '',
+  passwd: ''
+}
 
-    var enableSubmitBtn = function () {
-        vm.$refs.submitButton.disabled = form_is_invalid();
-    };
+var formIsInvalid = function () {
+  return !(eaterCredentials.login.length && eaterCredentials.passwd.length)
+}
 
-    var form_is_invalid = function() {
-        return !(eater_credentials.login.length && eater_credentials.passwd.length);
-    }
-
-    vm = new Vue({
-        el: "#app",
-
-        data: eater_credentials,
-        
-        created: function() {
-            console.log('Vue instance is created');
-        },
-
-        destroyed: function() {
-            console.log('Vue instance was destroyed');
-        },
-
-        watch: {
-            login: enableSubmitBtn,
-            passwd: enableSubmitBtn
-        },
-
-        methods: {
-            submit: function(){
-                console.log('Submit button clicked');
-                if (!form_is_invalid()) {
-                    axios.post('/api/login', {
-                        username: eater_credentials.login,
-                        password: eater_credentials.passwd
-                    })
-                    .then(function(response){
-                        if (!response.data.hasOwnProperty('result')) {
-                            console.log("Response without result...");
-                        }
-                        if (response.data.result == 'success') {
-                            console.log("All right, redirecting to home page");
-                            window.location.href = '/';
-                        }
-                        else {
-                            console.log("Wrong credentials!");
-                        }
-                    })
-                    .catch(function(error){
-                        console.log('Error: ' + error);
-                    })
-                }
-            }
+var submitForm = function () {
+  console.log('Submit button clicked')
+  if (!formIsInvalid()) {
+    axios.post('/api/login', {
+      username: eaterCredentials.login,
+      password: eaterCredentials.passwd
+    })
+      .then(function (response) {
+        if (!response.data.hasOwnProperty('result')) {
+          console.log('Response without result...')
         }
-    });
-};
+        if (response.data.result === 'success') {
+          console.log('All right, redirecting to home page')
+          window.location.href = '/'
+        } else {
+          console.log('Wrong credentials!')
+        }
+      })
+      .catch(function (error) {
+        console.log('Error: ' + error)
+      })
+  }
+}
+
+export { submitForm, eaterCredentials }
