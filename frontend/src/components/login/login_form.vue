1294e5a2ac50814b2f490ddd52d21dc53964506f
artemgorokhov <48455754+artemgorokhov@users.noreply.github.com>
2019-03-23 21:12:14 +0300

    Login page (#8)
    * First variant
    
    * Login page design

--- frontend/src/components/login/login_form.vue
+++ frontend/src/components/login/login_form.vue
@@ -1,25 +1,46 @@
 <template>
-    <form>
-    Login <input type="text" 
-                 v-model="login" 
-                 v-on:input="changeSubmitStatus"
-                 placeholder="LOGIN"/>
-            <br>
-    Password <input 
-                 type="text" 
-                 v-model="passwd" 
-                 v-on:input="changeSubmitStatus"
-                 placeholder="PASSWORD"/>
-            <br>
-    <button v-on:click="submit" 
-            type="button" 
-            class="btn" 
-            ref="submitButton" disabled>Submit</button>
-  </form>
+    <div class="box has-background-light
+                has-text-dark"
+          id="loginForm">
+        <table class="table is-fullwidth">
+            <thead>
+                <tr><td><span class="centered">
+                    <img src="@/assets/img/chef.svg" class="centered"/>
+                </span></td></tr>
+            </thead>
+            <tfoot>
+                <tr><td>
+                    <a v-on:click="submit"
+                       class="button is-success centered" 
+                       ref="submitButton" :disabled="!(login && passwd)">Join in</a>
+                </td></tr>
+            </tfoot>
+            <tbody>
+                <tr><td><div class="control has-icons-left">
+                    <input type="text" 
+                         class="input"
+                         v-model="login"
+                         placeholder="LOGIN"/>
+                    <span class="icon is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                </div></td></tr>
+                <tr><td><div class="control has-icons-left">
+                    <input type="password" 
+                         class="input"
+                         v-model="passwd" 
+                         placeholder="PASSWORD"/>
+                    <span class="icon is-left">
+                        <i class="fas fa-lock"></i>
+                    </span>
+                </div></td></tr>
+            </tbody>
+        </table>
+  </div>
 </template>
 
 <script>
-import { submitForm, formIsInvalid } from '@/assets/js/login_form.js'
+import { submitForm } from '@/assets/js/login_form.js'
 
 export default {
   name: 'login-form',
@@ -30,11 +51,28 @@ export default {
   methods: {
     submit: function() {
         submitForm(this.$data.login, this.$data.passwd);
-    },
-    changeSubmitStatus: function() {
-        this.$refs.submitButton.disabled = formIsInvalid(this.$data.login, this.$data.passwd);
     }
   }
 }
 
 </script>
+
+<style lang="sass">
+#loginForm
+    border: 1px solid gray
+    min-width: 50%
+    max-width: 60%
+
+#loginForm img
+    height: 2.5em
+
+#loginForm table
+    background-color: rgba(0, 0, 0, 0)
+#loginForm table tr td
+    border: none
+
+.centered
+    margin: auto
+    display: block
+
+</style>
