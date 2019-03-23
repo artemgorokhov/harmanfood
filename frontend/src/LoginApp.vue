8c08147f3cc1581dec8aa49faafc07b80cb42f2f
Artem Gorokhov <artem.gorohov@gmail.com>
2019-03-23 16:47:27 +0300

    First variant

--- frontend/src/LoginApp.vue
+++ frontend/src/LoginApp.vue
@@ -1,5 +1,27 @@
 <template>
-  <login-form/>
+    <section class="hero is-fullheight 
+                    has-background-dark">
+        <div class="hero-body is-paddingless">
+        <div class="container 
+                    columns
+                    is-fluid
+                    welcomeContainer">
+            <div class="column is-half 
+                        has-text-light
+                        has-text-right
+                        welcomeTitle">
+                <h1> {{ welcome_message }} </h1>
+                <h2 is-pulled-right> 
+                    {{ welcome_description }} 
+                </h2>
+            </div>
+            <div class="column
+                        has-text-dark">
+                <login-form/>
+            </div>
+        </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -7,6 +29,10 @@ import LoginForm from '@/components/login/login_form.vue'
 
 export default {
     name: 'LoginApp',
+    data: () => ({
+        welcome_message: "Добро пожаловать, едок!",
+        welcome_description: "Пожалуйста, представься.Так ты сможешь присоединиться к нашему скромному обеду."
+    }),
     components: {
         LoginForm
     }
@@ -18,7 +44,18 @@ export default {
 @import "@/assets/css/contrast_theme.scss"
 
 #loginApp
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+.welcomeContainer .column
+    padding: 2em;
+
+.welcomeTitle h1
+    font-size: 1.25em;
+    text-decoration: underline;
+
+.welcomeTitle h2
+    font-size: 0.75em;
+    padding-left: 25%;
 
 </style>
--- frontend/src/components/login/login_form.vue
+++ frontend/src/components/login/login_form.vue
@@ -1,20 +1,39 @@
 <template>
-    <form>
-    Login <input type="text" 
-                 v-model="login" 
-                 v-on:input="changeSubmitStatus"
-                 placeholder="LOGIN"/>
-            <br>
-    Password <input 
-                 type="text" 
-                 v-model="passwd" 
-                 v-on:input="changeSubmitStatus"
-                 placeholder="PASSWORD"/>
-            <br>
-    <button v-on:click="submit" 
-            type="button" 
-            class="btn" 
-            ref="submitButton" disabled>Submit</button>
+    <form class="has-background-light
+                 has-text-dark"
+          id="loginForm">
+        <table class="table is-fullwidth">
+            <thead>
+                <tr><td><div class="centered">
+                    <i class="far fa-user"></i>
+                </div></td></tr>
+            </thead>
+            <tfoot>
+                <tr><td>
+                    <button v-on:click="submit" 
+                        type="button" 
+                        class="btn centered" 
+                        ref="submitButton" disabled>Submit</button>
+                </td></tr>
+            </tfoot>
+            <tbody>
+                <tr><td>
+                    <input type="text" 
+                         class="centered"
+                         v-model="login" 
+                         v-on:input="changeSubmitStatus"
+                         placeholder="LOGIN"/>
+                </td></tr>
+                <tr><td>
+                    <input type="text" 
+                         class="centered"
+                         v-model="passwd" 
+                         v-on:input="changeSubmitStatus"
+                         placeholder="PASSWORD"/>
+                    
+                </td></tr>
+            </tbody>
+        </table>
   </form>
 </template>
 
@@ -38,3 +57,23 @@ export default {
 }
 
 </script>
+
+<style lang="sass">
+#loginForm
+    border: 1px solid gray;
+    max-width: 50%;
+
+#loginForm table
+    background-color: rgba(0, 0, 0, 0);
+#loginForm table tr td
+    border: none;
+
+#loginForm input
+    font-size: 1em;
+    width: 75%;
+
+.centered
+    margin: auto
+    display: block
+
+</style>
