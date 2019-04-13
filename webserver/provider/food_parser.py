8ad401aa662822301dce42a31737cdbd4d29d493
Artem Gorokhov <artem.gorohov@gmail.com>
2019-04-13 17:15:19 +0300

    Get pics and description, but need address and render

--- webserver/provider/food_parser.py
+++ webserver/provider/food_parser.py
@@ -2,7 +2,10 @@ from requests_html import HTMLSession
 
 
 delivery_categories_selector = 'h2[id^="anchor_"]'
-delivery_dish_selector = '#{}+.dish_list .product_title'
+delivery_dish_selector = '#{}+.dish_list>li'
+delivery_title_selector = '.product_title'
+delivery_pic_selector = '.main_img>img'
+delivery_detail_selector = '.dish_detail'
 
 result = []
 
@@ -12,9 +15,14 @@ r = session.get('https://nn.delivery-club.ru/srv/Wok__Go/#Kazanskoje_sh/')
 for category_el in r.html.find(delivery_categories_selector):
     food = r.html.find(delivery_dish_selector.format(category_el.attrs['id']))
     for food_el in food:
+        title = food_el.find(delivery_title_selector)[0]
+        pic = food_el.find(delivery_pic_selector)[0]
+        detail = food_el.find(delivery_detail_selector)[0]
         result.append({
                 'category': category_el.text,
-                'title': food_el.text
+                'title': title.text,
+                'pic': pic.attrs['src'],
+                'description': detail.text
             }
         )
 
